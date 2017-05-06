b248e89efa471c3464be1d944ac32ca2ebbc0a7d
Kokos <konradqcharczyk@gmail.com>
2017-05-06 14:21:14 +0200

    start of board

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,7 @@
 import model.Matrix;
 public class Main {	
 	public static void main(String[] args){
-		Matrix<Integer> temp = new Matrix<Integer>(8,8);
-		temp.fill(10);
-		temp.setField(0, 0, 99);
-		temp.printOut();
+		
+
 	}
 }
--- src/model/Board.java
+++ src/model/Board.java
@@ -5,7 +5,24 @@ public class Board {
 	
 	private int boardSize = 8;
 	
-	public Board(){
+	public Board(BoardSize size){
+		this.boardSize = size.getValue();
 		board = new Matrix<Field>(boardSize, boardSize);
+		board.fill(Field.EMPTY);
+		int index = boardSize/2 - 1;
+		setField(index, index, Field.BLACK);
+		setField(index, index + 1, Field.WHITE);
+		setField(index + 1, index + 1, Field.BLACK);
+		setField(index + 1, index, Field.WHITE);
 	}
+	
+	public void setField(int x,int y, Field field){
+		board.setField(x, y, field);
+	}
+	
+	public Field getField(int x, int y){
+		return board.getField(x, y);
+	}
+	
+	
 }
