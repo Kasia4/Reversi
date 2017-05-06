f956605c6a00ed59522eb0afb05fde1f77a27b44
Kokos <konradqcharczyk@gmail.com>
2017-05-06 13:23:33 +0200

    created matrix

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,7 +1,11 @@
 package game;
 
+import model.Matrix;
+
 public class Game {	
 	public static void main(String[] args){
-		System.out.println("gitara siema");
+		Matrix<Integer> temp = new Matrix<Integer>(8,8);
+		temp.fill(10);
+		System.out.println(temp.getField(5,5));
 	}
 }
