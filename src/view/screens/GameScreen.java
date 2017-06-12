4bc1e9c1d10620702db9399171a132913c010005
Karol Chęciński <karolchecinski@wp.pl>
2017-06-12 14:36:21 +0200

    Last move result fixed

--- src/view/screens/GameScreen.java
+++ src/view/screens/GameScreen.java
@@ -67,9 +67,10 @@ public class GameScreen extends Screen{
         debugBtn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) { 
-                game.makeMove(new Vector2(-1,-1));
-                heu.setPlayerPawn(Pawn.WHITE);
-                System.out.println(heu.heuristicTest(board));
+                //game.makeMove(new Vector2(-1,-1));
+                //heu.setPlayerPawn(Pawn.WHITE);
+                //System.out.println(heu.heuristicTest(board));
+            	game.undoMove();
                 boardView.update();
             }
         });
